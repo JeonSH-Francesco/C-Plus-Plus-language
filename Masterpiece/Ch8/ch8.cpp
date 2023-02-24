b8ca2995db7ecf213652c163cbca0a044fb02d42
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2023-02-24 14:31:03 +0900

    Update ch8.cpp

--- Masterpiece/Ch8/ch8.cpp
+++ Masterpiece/Ch8/ch8.cpp
@@ -1,51 +1,120 @@
 #include <iostream>
+
 using namespace std;
 
-class BaseArray {
-private:
-	int capacity; // 배열의 크기
-	int* mem; //정수 배열을 만들기 위한 메모리의 포인터
+class Printer {
+	string model;
+	string manufacturer;
+	int printedCount;
+	int availableCount;
 protected:
-	BaseArray(int capacity=100) {
-		this->capacity = capacity;
-		mem = new int[capacity];
+	Printer(string mod = "", string manuf = "", int a = 0) {
+		this->model = mod;
+		this->manufacturer = manuf;
+		this->availableCount = a;
+		this->printedCount = 0;
 	}
-	~BaseArray() { delete[] mem; }
-	void put(int index, int val) {
-		mem[index] = val;
+	bool isValidPrint(int pages) {
+		if (availableCount > pages) return true;
+		else return false;
 	}
-	int get(int index) { return mem[index]; }
-	int getCapacity() { return capacity; }
+	void print(int pages) {
+		if (isValidPrint(pages)) {
+			printedCount += pages;
+			availableCount -= pages;
+		}
+		else cout << "용지가 부족하여 프린트할 수 없습니다." << endl;
+	}
+	void showPrinter() { cout << model << ", " << manufacturer << ", 남은 종이 " << availableCount << "장"; }
 };
 
-class MyStack:public BaseArray {
-	int top;
+class InkJetPrinter : public Printer {
+	int availableInk;
 public:
-	MyStack(int capacity) : BaseArray(capacity) { top = -1; }
-	int capacity() { return getCapacity(); }
-	int length() { return top + 1;}
-	void push(int n) { put(++top, n); }
-	int pop() { return get(top--); }
+	InkJetPrinter(string mod = "", string manuf = "", int a = 0, int i = 0) : Printer(mod, manuf, a) 
+	{ this->availableInk = i; }
+	bool isValidInkJetPrint(int pages) {
+		if (availableInk > pages) return true;
+		else return false;
+	}
+	void printInkJet(int pages) {
+		if (isValidPrint(pages)) {
+			if (isValidInkJetPrint(pages)) {
+				print(pages);
+				availableInk -= pages;
+				cout << "프린트하였습니다" << endl;
+			}
+			else {
+				cout << "잉크가 부족하여 프린트할 수 없습니다." << endl;
+			}
+		}
+		else cout << "용지가 부족하여 프린트할 수 없습니다." << endl;
+	}
+	void showInkJetPrinter() {
+		showPrinter();
+		cout << ", 남은 토너 " << availableInk << endl;
+	}
 };
 
+class LaserPrinter : public Printer {
+	int availableToner;
+public:
+	LaserPrinter(string mod = "", string manuf = "", int a = 0, int t = 0) : Printer(mod, manuf, a) 
+	{ this->availableToner = t; }
+	bool isValidLaserPrint(int pages) {
+		if (availableToner > pages) return true;
+		else return false;
+	}
+	void printInkJet(int pages) {
+		if (isValidPrint(pages)) {
+			if (isValidLaserPrint(pages)) {
+				print(pages);
+				availableToner -= pages;
+				cout << "프린트하였습니다" << endl;
+			}
+			else {
+				cout << "토너가 부족하여 프린트할 수 없습니다." << endl;
+			}
+		}
+		else cout << "용지가 부족하여 프린트할 수 없습니다." << endl;
+	}
+	void showLaserPrinter() {
+		showPrinter();
+		cout << ", 남은 토너 " << availableToner << endl;
+	}
+};
 
 int main() {
-	MyStack mStack(100);
-	int n;
-	cout << "스택에 삽입할 5개의 정수를 입력하라>> ";
-	for (int i = 0; i < 5; i++) {
-		cin >> n;
-		mStack.push(n);
+	InkJetPrinter ink("Officejet V40", "HP", 5, 10);
+	LaserPrinter laser("SCX-6x45", "삼성전자", 3, 20);
+
+	cout << "현재 작동중인 2대의 프린터는 아래와 같다" << endl;
+	cout << "잉크젯 : ";
+	ink.showInkJetPrinter();
+	cout << "레이저 : ";
+	laser.showLaserPrinter();
+
+	int printer, paper;
+	char ch;
+	while (true) {
+		cout << endl << "프린터(1:잉크젯, 2:레이저)와 매수 입력>>";
+		cin >> printer >> paper;
+		switch (printer) {
+		case 1: ink.printInkJet(paper); break;
+		case 2: laser.printInkJet(paper); break;
+		default: break;
+		}
+		ink.showInkJetPrinter();
+		laser.showLaserPrinter();
+
+		cout << "게속 프린트 하시겠습니까(y/n)>>";
+		cin >> ch;
+		if (ch == 'n') break;
+		else continue;
 	}
-	cout << "스택의 용량:" << mStack.capacity() << ", 스택의 크기:" << mStack.length() << endl;
-	cout << "스택의 원소를 순서대로 제거하여 출력한다>> ";
-	while (mStack.length() != 0) {
-		cout << mStack.pop() << ' ';
-	}
-	cout << endl << "스택의 현재 크기 : " << mStack.length() << endl;
+
 	return 0;
 }
-
 
 
 /*
@@ -304,6 +373,169 @@ int main() {
 		cout << mStack.pop() << ' ';
 	}
 	cout << endl << "스택의 현재 크기 : " << mStack.length() << endl;
+	return 0;
+}
+-------
+7번문제
+#include <iostream>
+using namespace std;
+
+class BaseMemory {
+	char* mem;
+protected:
+	BaseMemory(int size) { mem = new char[size]; }
+	void setData(char x, int length) { mem[length] = x; }
+	void setData(char x[], int length) {
+		for (int i = 0; i < length; i++)
+			mem[i] = x[i];
+	}
+	char getData(int index)
+	{
+		return mem[index];
+	}
+};
+
+class ROM : public BaseMemory {
+public:
+	ROM(int size, char x[], int length) :BaseMemory(size) {
+		setData(x, length);
+	}
+	char read(int index) { return getData(index); }
+};
+
+
+class RAM : public BaseMemory {
+public:
+	RAM(int size) : BaseMemory(size) {}
+	void write(int index, char data) { setData(data, index); }
+	char read(int index) { return getData(index); }
+};
+
+
+int main() {
+	char x[5] = { 'h','e','l','l','o' };
+	ROM biosROM(1024 * 10, x, 5);
+	RAM mainMemory(1024 * 1024);
+
+	for (int i = 0; i < 5; i++) mainMemory.write(i, biosROM.read(i));
+	for (int i = 0; i < 5; i++) cout << mainMemory.read(i);
+	return 0;
+}
+--------
+8번문제
+#include <iostream>
+
+using namespace std;
+
+class Printer {
+	string model;
+	string manufacturer;
+	int printedCount;
+	int availableCount;
+protected:
+	Printer(string mo = "", string me = "", int a = 0) {
+		this->model = mo;
+		this->manufacturer = me;
+		this->availableCount = a;
+		this->printedCount = 0;
+	}
+	bool isValidPrint(int pages) {
+		if (availableCount > pages) return true;
+		else return false;
+	}
+	void print(int pages) {
+		if (isValidPrint(pages)) {
+			printedCount += pages;
+			availableCount -= pages;
+		}
+		else cout << "용지가 부족하여 프린트할 수 없습니다." << endl;
+	}
+	void showPrinter() { cout << model << ", " << manufacturer << ", 남은 종이 " << availableCount << "장"; }
+};
+
+class InkJetPrinter : public Printer {
+	int availableInk;
+public:
+	InkJetPrinter(string mo = "", string me = "", int a = 0, int i = 0) : Printer(mo, me, a) { this->availableInk = i; }
+	bool isValidInkJetPrint(int pages) {
+		if (availableInk > pages) return true;
+		else return false;
+	}
+	void printInkJet(int pages) {
+		if (isValidPrint(pages)) {
+			if (isValidInkJetPrint(pages)) {
+				print(pages);
+				availableInk -= pages;
+				cout << "프린트하였습니다" << endl;
+			}
+			else {
+				cout << "잉크가 부족하여 프린트할 수 없습니다." << endl;
+			}
+		}
+		else cout << "용지가 부족하여 프린트할 수 없습니다." << endl;
+	}
+	void showInkJetPrinter() {
+		showPrinter();
+		cout << ", 남은 토너 " << availableInk << endl;
+	}
+};
+
+class LaserPrinter : public Printer {
+	int availableToner;
+public:
+	LaserPrinter(string mo = "", string me = "", int a = 0, int t = 0) : Printer(mo, me, a) { this->availableToner = t; }
+	bool isValidLaserPrint(int pages) {
+		if (availableToner > pages) return true;
+		else return false;
+	}
+	void printInkJet(int pages) {
+		if (isValidPrint(pages)) {
+			if (isValidLaserPrint(pages)) {
+				print(pages);
+				availableToner -= pages;
+				cout << "프린트하였습니다" << endl;
+			}
+			else {
+				cout << "토너가 부족하여 프린트할 수 없습니다." << endl;
+			}
+		}
+		else cout << "용지가 부족하여 프린트할 수 없습니다." << endl;
+	}
+	void showLaserPrinter() {
+		showPrinter();
+		cout << ", 남은 토너 " << availableToner << endl;
+	}
+};
+
+int main() {
+	InkJetPrinter ink("Officejet V40", "HP", 5, 10);
+	LaserPrinter laser("SCX-6x45", "삼성전자", 3, 20);
+
+	cout << "현재 작동중인 2대의 프린터는 아래와 같다" << endl;
+	cout << "잉크젯 : ";
+	ink.showInkJetPrinter();
+	cout << "레이저 : ";
+	laser.showLaserPrinter();
+
+	int printer, paper;
+	char ch;
+	while (true) {
+		cout << endl << "프린터(1:잉크젯, 2:레이저)와 매수 입력>>";
+		cin >> printer >> paper;
+		switch (printer) {
+		case 1: ink.printInkJet(paper); break;
+		case 2: laser.printInkJet(paper); break;
+		default: break;
+		}
+		ink.showInkJetPrinter();
+		laser.showLaserPrinter();
+
+		cout << "게속 프린트 하시겠습니까(y/n)>>";
+		cin >> ch;
+		if (ch == 'n') break;
+		else continue;
+	}
+
 	return 0;
 }
 */
