925e9f22e560ae38714e5ac62d939ac1ff649ac9
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2023-02-27 21:05:57 +0900

    Update ch9.cpp

--- Masterpiece/Ch9/ch9.cpp
+++ Masterpiece/Ch9/ch9.cpp
@@ -1,143 +1,162 @@
 #include <iostream>
 using namespace std;
 
-class Printer { //추상 클래스
-	string model;// 모델
-	string manufacturer; // 제조사
-	int printedCount; //인쇄 매수
-	int availableCount; // 인쇄 종이 잔량
+class Shape {
+	Shape* next; //추상 클래스의 포인터 선언
 protected:
-	Printer(string mod = "", string manuf = "", int a = 0) {
-		this->model = mod;
-		this->manufacturer = manuf;
-		this->availableCount = a;
-		this->printedCount = 0;
+	virtual void draw() = 0;
+public:
+	Shape() { next = NULL; }
+	virtual ~Shape() {}
+	Shape* add(Shape* p) {
+		this->next = p;
+		return p;
 	}
-	bool isValidPrint(int pages) {
-		if (availableCount > pages) return true;
-		else return false;
+	void paint() {
+		draw();
 	}
-
-	string getModel() { return model; }
-	string getManufacturer() { return manufacturer; }
-	int getPrintedCount() { return printedCount; }
-	int getAvailableCount() { return availableCount; }
-
-	void setModel(string model) { this->model = model; }
-	void setManufacturer(string manufacturer) { this->manufacturer = manufacturer; }
-	void setPrintedCount(int printedCount) { this->printedCount = printedCount; }
-	void setAvailableCount(int availableCount) { this->availableCount = availableCount; }
-
-	virtual void print(int pages) = 0; //순수 가상 함수
-	virtual void show() = 0; //순수 가상 함수
+	Shape* getNext() { return next; }
 };
 
-class InkJetPrinter : public Printer {
-	int availableInk;
+//순수 가상함수 다양하게 구현
+class Line : public Shape {
+protected:
+	virtual void draw() {
+		cout << "Line" << endl;
+	}
+};
+
+class Circle : public Shape {
+protected:
+	virtual void draw() {
+		cout << "Circle" << endl;
+	}
+};
+
+class Rectangle : public Shape {
+protected:
+	virtual void draw() {
+		cout << "Rectangle" << endl;
+	}
+};
+
+class UI {
 public:
-	InkJetPrinter(string mod = "", string manuf = "", int a = 0, int i = 0) : Printer(mod, manuf, a)
-	{
-		this->availableInk = i;
+	static int getMenu() {
+		int key;
+		cout << "삽입:1, 삭제:2, 모두보기:3, 종료:4 >>";
+		cin >> key;
+		return key;
 	}
-
-	bool isValidInkJetPrint(int pages) {
-		if (availableInk > pages) return true;
-		else return false;
+	static int getShapeTypeToInsert() {
+		int key;
+		cout << "선:1, 원:2, 사각형:3 >>";
+		cin >> key;
+		return key;
 	}
-	int getAvailableInk() { return availableInk; }
-	void setAvailableInk(int availableInk) { this->availableInk = availableInk; }
-
-	void print(int pages) {
-		if (isValidPrint(pages)) {
-			if (isValidInkJetPrint(pages)) {
-				setPrintedCount(getPrintedCount() + pages);
-				setAvailableCount(getAvailableCount() - pages);
-				setAvailableInk(getAvailableInk() - pages);
-				cout << "프린트하였습니다" << endl;
-			}
-			else {
-				cout << "잉크가 부족하여 프린트할 수 없습니다." << endl;
-			}
-		}
-		else cout << "용지가 부족하여 프린트할 수 없습니다." << endl;
+	static int getShapeIndexToDelete() {
+		int key;
+		cout << "삭제하고자 하는 도형의 인덱스 >> ";
+		cin >> key;
+		return key;
 	}
-	void show() {
-		cout << getModel() << ", " << getManufacturer() << ", 남은 종이 " << getAvailableCount() << "장, 남은 잉크 " << getAvailableInk() << endl;
-	}
-
 };
 
 
-class LaserPrinter : public Printer {
-	int availableToner;
+
+class GraphicEditor {
+	Shape* pStart;
+	Shape* pLast;
 public:
-	LaserPrinter(string mod = "", string manuf = "", int a = 0, int t = 0) : Printer(mod, manuf, a)
-	{
-		this->availableToner = t;
+	GraphicEditor() { pStart = pLast = NULL; }
+	void InsertItem(int type) {
+		Shape* p = NULL;
+		switch (type) {
+		case 1:
+			p = new Line();
+			break;
+		case 2:
+			p = new Circle();
+			break;
+		case 3:
+			p = new Rectangle();
+			break;
+		default:
+			break;
+		}
+		if (pStart == NULL) {
+			pStart = p;
+			pLast = p;
+			return;
+		}
+		else {
+			pLast->add(p);
+			pLast = pLast->getNext();
+		}
 	}
 
-	bool isValidLaserPrint(int pages) {
-		if (availableToner > pages) return true;
-		else return false;
+	void deleteItem(int index) {
+		Shape* pre = pStart;
+		Shape* tmp = pStart;
+		if (pStart == NULL) {
+			cout << "도형이 없습니다." << endl;
+			return;
+		}
+		for (int i = 1; i < index; i++) {
+			pre = tmp;
+			tmp = tmp->getNext();
+		}
+		if (tmp == pStart) {  //처음 것 지워지는 경우
+			pStart = tmp->getNext();//단순히 tmp의 다음 노드 연결
+			delete tmp;
+		}
+		else { // 중간의 것 지워지는 경우
+			pre->add(tmp->getNext()); // 삭제한 다음 노드를 연결
+			delete tmp;
+		}
 	}
 
-	int getAvailableToner() { return availableToner; }
-	void setAvailableToner(int availableToner) { this->availableToner = availableToner; }
+	void show() {
+		Shape* tmp = pStart;
+		int i = 0;
+		while (tmp != NULL) {
+			cout << i++ << ": ";
+			tmp->paint();
+			tmp = tmp->getNext();
+		}
+	}
 
-	void print(int pages) {
-		if (isValidPrint(pages)) {
-			if (isValidLaserPrint(pages)) {
-				setPrintedCount(getPrintedCount() + pages);
-				setAvailableCount(getAvailableCount() - pages);
-				setAvailableToner(getAvailableToner() - pages);
-				cout << "프린트하였습니다" << endl;
-			}
-			else {
-				cout << "토너가 부족하여 프린트할 수 없습니다." << endl;
+	void run() {
+		cout << "그래픽 에디터입니다." << endl;
+		int menu, index, type;
+		while (true) {
+			menu = UI::getMenu();
+			switch (menu) {
+			case 1:
+				type = UI::getShapeTypeToInsert();
+				InsertItem(type);
+				break;
+			case 2:
+				index = UI::getShapeIndexToDelete();
+				deleteItem(index);
+				break;
+			case 3:
+				show();
+				break;
+			default:
+				return;
 			}
 		}
-		else cout << "용지가 부족하여 프린트할 수 없습니다." << endl;
 	}
-	void show() {
-		cout << getModel() << ", " << getManufacturer() << ", 남은 종이 " << getAvailableCount() << "장, 남은 토너 " << availableToner << endl;
-	}
-
 };
 
 int main() {
-	InkJetPrinter* ink = new InkJetPrinter("Officejet V40", "HP", 5, 10);
-	LaserPrinter* laser = new LaserPrinter("SCX-6x45", "삼성전자", 3, 20);
-
-	cout << "현재 작동중인 2대의 프린터는 아래와 같다" << endl;
-	cout << "잉크젯 : ";
-	ink->show();
-
-	cout << "레이저 : ";
-	laser->show();
-
-	int printer, paper;
-	char ch;
-
-	while (true) {
-		cout << endl << "프린터(1.잉크젯,2:레이저)와 매수 입력>>";
-		cin >> printer >> paper;
-		switch (printer) {
-		case 1: ink->print(paper); break;
-		case 2: laser->print(paper); break;
-		default: break;
-		}
-		ink->show();
-		laser->show();
-
-		cout << "계속 프린트 하시겠습니까(y/n)>>";
-		cin >> ch;
-		if (ch == 'n') break;
-		else continue;
-	}
-
+	GraphicEditor graphicEditor;
+	graphicEditor.run();
 
 	return 0;
 }
+
 
 /*#include <iostream>
 using namespace std;
