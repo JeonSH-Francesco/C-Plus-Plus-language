67a5b1671349512c32a8fd6e5ba072c831f19fa9
전승호 <112309895+JeonSH-Francesco@users.noreply.github.com>
2023-03-08 16:52:23 +0900

    Update and rename file_read.cpp to file_read_1.cpp

--- Masterpiece/Ch12/file_read_1.cpp
+++ Masterpiece/Ch12/file_read_1.cpp
@@ -4,40 +4,34 @@
 #include <vector>
 using namespace std;
 
- // fin 스트림으로부터 벡터 v에 읽어들임
-void fileRead(vector<string>& v, ifstream& fin) {
+
+void fileRead(vector<string>& v, ifstream& fin) { // fin 스트림으로부터 벡터 v에 읽어들임
 	string line;
-  
 	while (getline(fin, line)) { // fin 파일에서 한 라인 읽기
 		v.push_back(line); // 읽은 라인을 벡터에 저장
 	}
-  
 }
 
- // 벡터 v에서 word를 찾아 출력
-void search(vector<string>& v, string word) {
-  
+void search(vector<string>& v, string word) { // 벡터 v에서 word를 찾아 출력
 	for (int i = 0; i < v.size(); i++) {
 		int index = v[i].find(word);
 		if (index != -1) // found
 			cout << v[i] << endl;
 	}
-  
 }
 
 int main() {
 	vector<string> wordVector;
-	ifstream fin("경로명 파일명"); //사용자 맞게 수정 필요
+	ifstream fin("C:\\tempC++\\words.txt");
 
 	if (!fin) {
-		cout << "파일을 열 수 없습니다" << endl;
+		cout << "words.txt 파일을 열 수 없습니다" << endl;
 		return 0; // 열기 오류
 	}
-  
 	fileRead(wordVector, fin); // fin 스트림으로부터 wordVector에 라인 별로 읽기
 	fin.close();
 
-	cout << "파일을 읽었습니다." << endl;
+	cout << "words.txt 파일을 읽었습니다." << endl;
 	while (true) {
 		cout << "검색할 단어를 입력하세요 >>";
 		string word;
